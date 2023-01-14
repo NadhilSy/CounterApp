7e566f61bf5130320671a651082da3ba61b695aa
Muhamad Nadhil Syarif <nadhil.dev@gmail.com>
2023-01-14 13:24:23 +0700

    commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
+import React from "react";
 
 export default function App() {
+  const [counter, setCounter] = React.useState(0);
+  const increment = () => setCounter(counter + 1);
+
+  const decrement = () => setCounter(counter - 1);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Counter</Text>
+      <Text style={styles.counter}>{counter}</Text>
+      <View style={styles.buttonWrapper}>
+        {/*<Button title=Increment onPress={increment}/>*/}
+        {/*<Button title=Decremen onPress={decrement}/>*/}
+        <Pressable onPress={increment} style={styles.button1}>
+          <Text style={styles.textButton}>+</Text>
+        </Pressable>
+        <Pressable onPress={decrement} style={styles.button2}>
+          <Text style={styles.textButton}>-</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -17,4 +33,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  counter:{
+    fontSize : 50,
+    fontWeight : 'bold'
+  },
+  buttonWrapper:{
+    marginTop: 20,
+    flexDirection: "row"
+  },
+  button1:{
+    height: 55,
+    width: 55,
+    backgroundColor:"#a692e8",
+    margin:10,
+    padding: 15,
+    borderRadius: 50
+  },
+  button2:{
+    height: 55,
+    width: 55,
+    backgroundColor:"#e83158",
+    margin:10,
+    padding: 15,
+    borderRadius: 50
+  },
+  textButton: {
+    fontStyle:"bold",
+    fontSize:17,
+    color: "white",
+    textAlign:"center"
+  }
 });
